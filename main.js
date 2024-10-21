4481bd33a3185d334aa72dd0a9338876d105f73c
professor-weslly <127995812+professor-weslly@users.noreply.github.com>
2024-10-21 11:34:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,32 @@
     const campoPalpite = document.querySelector('.campoPalpite');
     let contagemPalpites = 1;
     let botaoReiniciar;
+
+function verificarPalpite() {
+  const palpiteUsuario = Number(campoPalpite.value);
+  if (contagemPalpites === 1) {
+    palpites.textContent = "Palpites anteriores: ";
+  }
+
+palpites.textContent += palpiteUsuario + " ";
+
+  if (palpiteUsuario === numeroAleatorio) {
+    ultimoResultado.textContent = "Parabéns! Você acertou!";
+    ultimoResultado.style.backgroundColor = "green";
+    baixoOuAlto.textContent = "";
+    finalizarJogo();
+    } else if (contagemPalpites === 10) {
+      ultimoResultado.textContent = "FIM DE JOGO!!!";
+      baixoOuAlto.textContent = "";
+      finalizarJogo();
+  } else {
+    ultimoResultado.textContent = "Errado";
+    ultimoResultado.style.backgroundColor = "red";
+    if (palpiteUsuario < numeroAleatorio) {
+    baixoOuAlto.textContent = "O último palpite foi muito baixo";
+  } else if (palpiteUsuario > numeroAleatorio) {
+    baixoOuAlto.textContent = "O último palpite foi muito alto";
+    }
+  }
+  
+}
